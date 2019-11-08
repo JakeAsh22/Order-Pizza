2c6548234df25d6d3476053f8fcbb9a260f40d02
JakeAsh22 <jacob.ash1998@gmail.com>
2019-11-08 13:15:01 -0800

    Make jquery inputs for size and toppings, working on how to make the toppings save into array properly

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,36 @@ function Pizza(size,toppings)
 }
 Pizza.prototype.cost = function()
 {
-  if (this.size==1)
+  if (this.size=="small")
     this.price = 5;
-  else if (this.size == 2)
+  else if (this.size == "medium")
     this.price = 7;
-  else if (this.size == 3)
+  else if (this.size == "large")
     this.price = 10;
   else
     this.price = 12;
   for(var i=0;i<this.toppings.length;i++)
   {
-    if(this.toppings[i]=="cheese"){
+    if(this.toppings[i]=="cheese")
       this.price += 2;
-    }
-    else if(this.toppings[i]=="pepperoni"){
+    else if(this.toppings[i]=="pepperoni")
       this.price += 3;
+    else if(this.toppings[i]=="sausage")
+      this.price += 2;
+    else if (this.toppings[i]=="sausage")
+      this.price += 2.50;
   }
-  else if(this.toppings[i]=="sausage"){
-    this.price += 2;
-  }
-}
-return this.price;
+  return this.price;
 }
 var pizza1 = new Pizza (1,["cheese", "pepperoni"]);
+$(document).ready(function()
+{
+  $("form#pizzaForm").submit(function(event)
+  {
+    event.preventDefault();
+    var size = $("input[name = 'pSize']:checked").val();
+    var toppings = [];
+    toppings = $("input[name = 'toppings']:checked").val();
+    console.log(size , toppings[1]);
+  });
+});
